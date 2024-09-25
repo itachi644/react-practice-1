61dc27bc9506690d3f0bfb95b9a74ec64eb09fc7
itachi644 <brohuzaif2008@gmail.com>
2024-09-25 19:40:50 +0530

    practice

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <profile />
+      <h1>profile</h1>
+      <div className='profile'>
+      <p className='name'>huzaif</p>
+      <img className='profile-img' src="https://www.motoroids.com/wp-content/uploads/2015/04/Fast-and-Furious-Toyota-Supra-10.jpg" alt="" />
+      <p className='about'>Im a student</p>
+      </div>
     </div>
+   
   );
 }
 

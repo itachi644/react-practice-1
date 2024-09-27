8d4bddb6b1c1d28927b41dd660b6a6d6faf32faa
itachi644 <brohuzaif2008@gmail.com>
2024-09-27 19:34:46 +0530

    practice

--- practice/src/App.js
+++ practice/src/App.js
@@ -5,15 +5,9 @@ import Profile from './components/Profile';
 function App() {
   return (
     <div>
-      <profile />
       <h1>profile</h1>
-      <div className='profile'>
-      <p className='name'>huzaif</p>
-      <img className='profile-img' src="https://www.motoroids.com/wp-content/uploads/2015/04/Fast-and-Furious-Toyota-Supra-10.jpg" alt="" />
-      <p className='about'>Im a student</p>
-      </div>
+      <Profile />
     </div>
-   
   );
 }
 
--- practice/src/components/profile.js
+++ practice/src/components/profile.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Profile = () => {
     return (
-        <h1>profile</h1>
+        <div className='profile'>
+            <p className='name'>huzaif</p>
+            <img className='profile-img' src="https://www.motoroids.com/wp-content/uploads/2015/04/Fast-and-Furious-Toyota-Supra-10.jpg" alt="" />
+            <p className='about'>Im a student</p>
+        </div>
+        
     )
 }
 export default Profile;

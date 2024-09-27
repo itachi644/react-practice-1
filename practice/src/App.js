58fc946038d2e6aae0b7411e55dd39072cfee053
itachi644 <brohuzaif2008@gmail.com>
2024-09-27 19:37:29 +0530

    practice

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,10 +3,30 @@ import './App.css';
 import Profile from './components/Profile';
 
 function App() {
+  const profiles = [{
+    name:"huzaif",
+    img:"https://img.etimg.com/thumb/width-1200,height-900,imgsize-101990,resizemode-75,msid-93641036/industry/renewables/gas-powered-muscle-cars-drive-into-the-sunset-turn-electric.jpg",
+    about:"i'm a student"
+ 
+  },
+  {
+    name:"samit",
+    img:"https://w7.pngwing.com/pngs/411/398/png-transparent-cartoon-driving-vehicle-funny-cars-convertible-driving-hand-thumbnail.png",
+    about:"i'm a student"
+  },
+    {
+      name:"rehan",
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrkg3hpLfoaq2F8RD2ILtDmgk_2LNSatY5Mg&s",
+      about:"i'm a student"
+  }];
   return (
     <div>
-      <h1>profile</h1>
-      <Profile />
+      <h1>profile</h1>{
+          profiles.map((profile, key) => {
+          return <Profile key={key} name={profile.name} imgUrl={profile.img} about={profile.about}/>    
+        })
+      } 
+      
     </div>
   );
 }
--- practice/src/components/profile.js
+++ practice/src/components/profile.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-const Profile = () => {
+const Profile = ({name,imgUrl,about}) => {
     return (
         <div className='profile'>
-            <p className='name'>huzaif</p>
-            <img className='profile-img' src="https://www.motoroids.com/wp-content/uploads/2015/04/Fast-and-Furious-Toyota-Supra-10.jpg" alt="" />
-            <p className='about'>Im a student</p>
-        </div>
+            <p className='name'>profile name is {name}</p>
+            <img className='profile-img' src={imgUrl} alt={imgUrl} />
+            <p className='about'>{about}</p>      
+        </div> 
         
     )
 }
 export default Profile;
+                
